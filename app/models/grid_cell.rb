2bfeeece11ae6493d04f8f6c4b7e3c54bf9de377
Steven Jackson <steve@sjackson.net>
2018-01-14 16:57:19 +0800

    Increase detection radius to 250meters
    GPS inaccuracy with chinese coordinates kinda sucks, so we're increasing
    it to a higher number to account for this. So that show-location's
    marker is always in a visible area.

--- app/models/grid_cell.rb
+++ app/models/grid_cell.rb
@@ -1,5 +1,5 @@
 class GridCell < ApplicationRecord
-  GRID_CELL_COVERAGE_RADIUS = 100 # unit: meters
+  GRID_CELL_COVERAGE_RADIUS = 250 # unit: meters
   validates :lonlat, presence: true
 
   def self.mark_grid_cells_as_visited_by_user!(longitude, latitude)
