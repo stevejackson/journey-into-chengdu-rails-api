ef1ab5af8516e415cebaf20b32a344434d8f90bd
Steven Jackson <steve@sjackson.net>
2018-01-14 17:43:20 +0800

    Show percent of chengdu explored in user metrics api

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,19 @@
 class UsersController < ApplicationController
   def dashboard_metrics
     render json: {
-      percentOfChengduExplored: 22.58,
+      percentOfChengduExplored: percent_of_chengdu_explored,
       landmarksUnlocked: 5,
       totalLandmarks: 15
     }
+  end
+
+  private
+
+  def percent_of_chengdu_explored
+    user_visited = UserGridCell.where(visited: true).count
+    total_cells = GridCell.count
+
+    percent = (user_visited.to_f / total_cells.to_f) * 100.0
+    percent.round(2)
   end
 end
